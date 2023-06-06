4b8a3e892a8b0aa73e2b31eb2477dc06ed9d13b4
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-06-06 12:57:37 +0300

    update all script.js

--- task 09/script.js
+++ task 09/script.js
@@ -1,16 +1,15 @@
 // Дано натуральне число. Знайти та вивести на сторінку всі його дільники.
 
-let num = parseInt(prompt("Введіть число:"))
-let dif = ""
+let num = parseInt(prompt("Введіть число:"));
+let dif = "";
 
 if (isNaN(num) || num === "" || num === null) {
   alert("Щось пішло не так");
-}else{
-  for(i = 1; i <= num; i ++){
-    if (num % i === 0){
-      dif += `${i}, `
+} else {
+  for (i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      dif += `${i}, `;
     }
   }
-  alert(`Дільники числа ${num}:\n${dif.slice(0, -2)}`)
+  alert(`Дільники числа ${num}:\n${dif.slice(0, -2)}`);
 }
-
--- task 11/script.js
+++ task 11/script.js
@@ -1,17 +1,17 @@
 // Дано натуральне число. Знайти суму його парних дільників
 
-let num = parseInt(prompt("Введіть число:"))
-let sumOddDif = 0
+let num = parseInt(prompt("Введіть число:"));
+let sumOddDif = 0;
 
 if (isNaN(num) || num === "" || num === null) {
   alert("Щось пішло не так");
-}else{
-  for(i = 1; i <= num; i ++){
-    if (num % i === 0){
-      if (i % 2 === 0){
-        sumOddDif += i
+} else {
+  for (i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      if (i % 2 === 0) {
+        sumOddDif += i;
       }
     }
   }
-  alert(`Сума парних дільників числа ${num}: ${sumOddDif}`)
+  alert(`Сума парних дільників числа ${num}: ${sumOddDif}`);
 }
