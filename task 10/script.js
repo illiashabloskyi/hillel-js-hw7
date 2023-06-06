d69c5d112827bc670db9dfd8592b7024ab79b25a
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-06-06 12:55:58 +0300

    update task11/script.js

--- task 10/script.js
+++ task 10/script.js
@@ -1,18 +1,17 @@
 // Дано натуральне число. Визначити кількість його парних дільників
 
-let num = parseInt(prompt("Введіть число:"))
-let sumOddDif = 0
+let num = parseInt(prompt("Введіть число:"));
+let countOddDif = 0;
 
 if (isNaN(num) || num === "" || num === null) {
   alert("Щось пішло не так");
-}else{
-  for(i = 1; i <= num; i ++){
-    if (num % i === 0){
-      if (i % 2 === 0){
-        sumOddDif ++
+} else {
+  for (i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      if (i % 2 === 0) {
+        countOddDif++;
       }
     }
   }
-  console.log(sumOddDif)
-  alert(`Кількість парних дільників числа ${num}: ${sumOddDif}`)
+  alert(`Кількість парних дільників числа ${num}: ${countOddDif}`);
 }
